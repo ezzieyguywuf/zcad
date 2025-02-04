187a6133abc973077b506fb0c68d892341eeab12
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2025-02-04 14:55:52 -0500

    Draw a triangle.
    TODO: clean this up, break into seperate files, etc.
    
    Also, this only draws a triangle in vulkan using wayland, so TODO make
    it cross platform or w/e.

--- build.zig
+++ build.zig
@@ -1,8 +1,21 @@
 const std = @import("std");
 
+const Scanner = @import("zig-wayland").Scanner;
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    const scanner = Scanner.create(b, .{});
+    const wayland = b.createModule(.{ .root_source_file = scanner.result });
+    scanner.addSystemProtocol("stable/xdg-shell/xdg-shell.xml");
+    scanner.generate("wl_compositor", 1);
+    scanner.generate("wl_shm", 1);
+    scanner.generate("xdg_wm_base", 1);
+
+    const vulkan = b.dependency("vulkan_zig", .{
+        .registry = b.dependency("vulkan_headers", .{}).path("registry/vk.xml"),
+    }).module("vulkan-zig");
 
     const exe = b.addExecutable(.{
         .name = "zcad",
@@ -11,15 +24,17 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    b.installArtifact(exe);
+    exe.root_module.addImport("wayland", wayland);
+    exe.root_module.addImport("vulkan", vulkan);
 
     // TODO: do this.
     // if (b.systemIntegrationOption("zcad", .{})) {
-    exe.linkSystemLibrary("glfw");
     exe.linkSystemLibrary("vulkan");
     exe.linkSystemLibrary("wayland-client");
 
     exe.linkLibC();
+
+    b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
 
@@ -30,6 +45,28 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const vert_cmd = b.addSystemCommand(&.{
+        "glslc",
+        "--target-env=vulkan1.2",
+        "-o",
+    });
+    const vert_spv = vert_cmd.addOutputFileArg("vert.spv");
+    vert_cmd.addFileArg(b.path("shaders/triangle.vert"));
+    exe.root_module.addAnonymousImport("vertex_shader", .{
+        .root_source_file = vert_spv,
+    });
+
+    const frag_cmd = b.addSystemCommand(&.{
+        "glslc",
+        "--target-env=vulkan1.2",
+        "-o",
+    });
+    const frag_spv = frag_cmd.addOutputFileArg("frag.spv");
+    frag_cmd.addFileArg(b.path("shaders/triangle.frag"));
+    exe.root_module.addAnonymousImport("fragment_shader", .{
+        .root_source_file = frag_spv,
+    });
 
     const test_filter = b.option([]const u8, "test-filter", "Filter for test");
     const exe_unit_tests = b.addTest(.{
