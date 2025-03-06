fb2a931e6d58ed3a10dba534ea72aee818a27c50
Wolfgang Sanyer <wolfgangesanyer@gmail.com>
2025-03-06 02:01:17 +0000

    Create rendered lines from zcad lines

--- src/main.zig
+++ src/main.zig
@@ -204,26 +204,15 @@ pub fn main() !void {
     // };
     // const point_indices = [_]u32{ 0, 1, 2 };
 
-    // const vk_line_vertices = makeLine(
-    //     .{ -5, -5, 0 },
-    //     .{ 5, -5, 0 },
-    //     .{ 0, 0, 0 },
-    //     .{ 0, 0, 0 },
-    // ) ++ makeLine(
-    //     .{ 5, -5, 0 },
-    //     .{ 5, 5, 0 },
-    //     .{ 0, 0, 0 },
-    //     .{ 0, 0, 0 },
-    // ) ++ makeLine(
-    //     .{ 5, -5, 0 },
-    //     .{ 5, -5, -10 },
-    //     .{ 0, 0, 0 },
-    //     .{ 0, 0, 0 },
-    // );
-    // const line_indices = [6]u32{ 0, 1, 2, 0, 3, 1 } ++ .{ 4, 5, 6, 4, 7, 5 } ++ .{ 8, 9, 10, 8, 11, 9 };
+    var rendered_lines = RenderedLines.init();
+    defer rendered_lines.deinit(allocator);
+    try rendered_lines.addLine(allocator, .{
+        .p0 = .{ .x = 0, .y = 0, .z = 0 },
+        .p1 = .{ .x = 10, .y = 10, .z = 10 },
+    });
 
     // try renderer.uploadInstanced(vkr.Vertex, &vk_ctx, .Points, &vk_point_vertices, &point_indices);
-    // try renderer.uploadInstanced(vkr.Line, &vk_ctx, .Lines, &vk_line_vertices, &line_indices);
+    try renderer.uploadInstanced(vkr.Line, &vk_ctx, .Lines, rendered_lines.vulkan_vertices.items, rendered_lines.vulkan_indices.items);
     // try renderer.uploadInstanced(vkr.Vertex, &vk_ctx, .Triangles, &vk_triangle_vertices, &triangle_indices);
 
     {
@@ -259,6 +248,84 @@ pub fn main() !void {
 
     std.debug.print("exiting main\n", .{});
 }
+
+pub const RenderedLines = struct {
+    // TODO: maybe make this an ArrayList(vkr.Line, void) to dedupe
+    vulkan_vertices: std.ArrayListUnmanaged(vkr.Line),
+    vulkan_indices: std.ArrayListUnmanaged(u32),
+
+    pub fn init() RenderedLines {
+        return RenderedLines{
+            .vulkan_vertices = .{},
+            .vulkan_indices = .{},
+        };
+    }
+
+    pub fn deinit(self: *RenderedLines, allocator: std.mem.Allocator) void {
+        self.vulkan_vertices.deinit(allocator);
+        self.vulkan_indices.deinit(allocator);
+    }
+
+    pub fn addLine(self: *RenderedLines, allocator: std.mem.Allocator, line: Line) !void {
+        const left = [3]f32{
+            @floatFromInt(line.p0.x),
+            @floatFromInt(line.p0.y),
+            @floatFromInt(line.p0.z),
+        };
+        const right = [3]f32{
+            @floatFromInt(line.p1.x),
+            @floatFromInt(line.p1.y),
+            @floatFromInt(line.p1.z),
+        };
+        const color = [3]f32{ 0, 0, 0 };
+
+        // we need to know how many vertices we have before we add any
+        const n: u32 = @intCast(self.vulkan_vertices.items.len);
+
+        // The line will consist of two triangles. First, define the four
+        // corners
+        try self.vulkan_vertices.append(allocator, .{
+            .posA = left,
+            .posB = right,
+            .left = true,
+            .up = false,
+            .colorA = color,
+            .colorB = color,
+        });
+        try self.vulkan_vertices.append(allocator, .{
+            .posA = left,
+            .posB = right,
+            .left = false,
+            .up = true,
+            .colorA = color,
+            .colorB = color,
+        });
+        try self.vulkan_vertices.append(allocator, .{
+            .posA = left,
+            .posB = right,
+            .left = true,
+            .up = true,
+            .colorA = color,
+            .colorB = color,
+        });
+        try self.vulkan_vertices.append(allocator, .{
+            .posA = left,
+            .posB = right,
+            .left = false,
+            .up = false,
+            .colorA = color,
+            .colorB = color,
+        });
+
+        // Next, make sure we index them in the correct order
+        try self.vulkan_indices.append(allocator, n);
+        try self.vulkan_indices.append(allocator, n + 1);
+        try self.vulkan_indices.append(allocator, n + 2);
+        try self.vulkan_indices.append(allocator, n);
+        try self.vulkan_indices.append(allocator, n + 3);
+        try self.vulkan_indices.append(allocator, n + 1);
+    }
+};
 
 fn makeLine(
     left_pos: [3]f32,
